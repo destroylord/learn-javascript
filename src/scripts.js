ac627c8deb3ba12695b23fd4b7fa9b24a6b4be46
destroylord <masapin68@gmail.com>
2020-11-03 10:46:19 +0700

    class

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,30 @@
+// Sysntact
+/*
+    class ClassName {
+    constructor() { ... }
+    method_1() { ... }
+    method_2() { ... }
+    method_3() { ... }
+    }
+    }
+*/ 
+
+class Members {
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+     age () {
+         let date = new Date();
+         return date.getFullYear() - this.year;
+     }
+}
+
+let mySelf = new Members('Dafrin',2001);
+document.getElementById('classes').innerHTML = "My Self is "+ mySelf.age() +" years old";
+console.log(mySelf);
+
+
 var carName = "Volvo";
 // arrays 
 var cars = ["ayla","kijang","toshiba"];
@@ -22,14 +49,13 @@ document.getElementById('demo3').innerHTML =  person.firstName + " is Years age 
     document.getElementById('escape').innerHTML = "We are the so-called \"Vikings\" from the north.";
     // ex : We are the so-called "Vikings" from the north.
 
-
 // function myFunction{} => default
 
 // arrow function
 var myFunction = () => {
     document.getElementById('demo').style.display = "block";
     document.getElementById('demo').style.fontSize = "32px";
-    document.getElementById('demo').style.fontFamily = 'Montserrat', sans-serif;   
+    document.getElementById('demo').style.fontFamily = 'Montserrat', sans-serif;  
 }
 
 myFunction();
